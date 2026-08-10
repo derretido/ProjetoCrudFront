272771f91b9315261ddc87378f4dbdc832ff9b3d
Leonardo <gato.raivoso.daniel@gmail.com>
2026-08-10 15:34:33 -0300

    ..

--- service.js
+++ service.js
@@ -101,8 +101,8 @@ async function carregarMedicos() {
     const medicos = await resposta.json();
     medicos.forEach(medico => {
         const option = document.createElement("option");
-        option.value = medico.ID_MED_CRM;
-        option.textContent = `${medico.ID_MED_CRM} - ${medico.meD_NOME_COMPLETO}`;
+        option.value = medico.iD_MED_CRM;
+        option.textContent = `${medico.iD_MED_CRM} - ${medico.meD_NOME_COMPLETO}`;
         select.appendChild(option);
     });
 }
